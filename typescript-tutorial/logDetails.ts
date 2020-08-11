7e5b1e9ce479b86abd49ca2866e28eca5b3b00f7
Gustavo Aguiar <gustavodfaguiar@gmail.com>
2020-08-10 21:10:25 -0300

    07. Type Aliases e Union - Mini-curso de TypeScript

---
+++ typescript-tutorial/logDetails.ts
@@ -0,0 +1,24 @@
+// type alias
+type Uid = number | string
+
+function logDetails(uid: Uid, item: string) {
+    console.log(`A product with ${uid} has a title as ${item}.`)
+}
+
+function logInfo(uid: Uid, user: string) {
+    console.log(`An user with ${uid} has a name as ${user}.`)
+}
+
+type Platform = 'Windows'| 'Linux' | 'Mac Os'
+
+function renderPlatform(platform: Platform) {
+    return platform
+}
+
+renderPlatform('Linux')
+
+logDetails(123, "sapato")
+logDetails("123", "sapato")
+
+logInfo(123, "Gustavo")
+logInfo("123", "Gustavo")
